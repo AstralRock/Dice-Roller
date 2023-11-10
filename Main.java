d67a754b54b6fa14ca7b222e5766ef39f3ea8586
AstralRock <purplerock408@gmail.com>
2023-11-09 23:22:00 -0800

    Refactor GUI layout and add character input and main buttons

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
 import java.awt.*;
 import java.io.*;
 import java.awt.event.*;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 
@@ -44,7 +45,7 @@ class frame extends JFrame
 
     public Dimension windowSize() {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        return new Dimension((int) (screenSize.height * PERCENT_HEIGHT * 3/2), (int) (screenSize.height * PERCENT_HEIGHT));
+        return new Dimension((int) (screenSize.height * PERCENT_HEIGHT * 3/4), (int) (screenSize.height * PERCENT_HEIGHT));
     }
 
     public class TerminalPanel extends JPanel {
@@ -61,11 +62,15 @@ class frame extends JFrame
 
         public void appendText(String text) {
             textArea.append(text);
+            // Scroll to the end of the text area
+            textArea.setCaretPosition(textArea.getDocument().getLength());
         }
 
         public void clear() {
             textArea.setText("");
         }
+
+        
 
         public void redirectSystemStreams() {
             OutputStream out = new OutputStream() {
@@ -110,44 +115,96 @@ class frame extends JFrame
     }
 
     public class mainPanel extends JPanel {
-        private JTextField textField;
 
         public mainPanel() {
-            super(new BorderLayout());
-            add(new JLabel("Hello World"), BorderLayout.CENTER);
             setPreferredSize(new Dimension((int)(windowSize().width * 0.9), windowSize().height));
-            add(createTextField(), BorderLayout.SOUTH);
+            setBackground(new Color(255, 230, 230));
+            setLayout(new BorderLayout());
+            add(characterInput(), BorderLayout.NORTH);
+            add(mainButtons(), BorderLayout.CENTER);
+            add(textFeildMania(), BorderLayout.SOUTH);
         }
+        public JPanel characterInput() {
+            JPanel panel = new JPanel();
+            panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS)); // Set layout to BoxLayout
+            panel.setBackground(new Color(230, 255, 230));
 
-        private JTextField createTextField() {
-            textField = new JTextField();
-            textField.addActionListener(new ActionListener() {
+            JLabel Jl = new JLabel("Enter Character Name: ");
+            Jl.setFont(new Font("Papyrus", Font.BOLD, (int)(PERCENT_HEIGHT*50)));
+            panel.add(Jl);
+
+            JTextField Jt = new JTextField(10);
+            Jt.setFont(new Font("Arial", Font.PLAIN, 40));
+            Jt.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    String input = textField.getText();
-                    System.out.println("User input: " + input);
-                    textField.setText("");
+                    System.out.println(Jt.getText());
+                    
                 }
             });
-            int textFieldHeight = (int) (getHeight() * 0.05);
-            int textFieldWidth = textFieldHeight * 3;
-            int textFieldX = (int) (getWidth() * 0.1);
-            int textFieldY = (int) (getHeight() * 0.1);
-            textField.setBounds(textFieldX, textFieldY, textFieldWidth, textFieldHeight);
-            return textField;
+            panel.add(Jt);
+            return panel;
         }
+
+        public JPanel mainButtons() {
+            JPanel panel = new JPanel(new GridLayout(5, 3, 10, 10)); // Use GridLayout to create 3 columns, with 10 pixels of horizontal and vertical gap
+            panel.setBackground(new Color(230, 230, 255));
+            String[][] buttons ={{"Melee","Stealth","Ranged"},
+                                 {"Spell","Lock","Tech"},
+                                 {"Dodge","Heal","Persuade"},
+                                 {"Control","Coin Flip","roll"},
+                                 {"Hack 1","Hack 2","Hack 3"}};
+            Color[] buttonColors = {new Color(237, 149, 149), new Color(149, 237, 167), new Color(149, 168, 237), new Color(203, 149, 237), new Color(237, 149, 214)};
+            
+            for (int i = 0; i < buttons.length; i++) {
+                for (int j = 0; j < buttons[i].length; j++) {
+                    JButton button = new JButton(buttons[i][j]);
+                    button.setFont(new Font("Papyrus", Font.BOLD, (int)(PERCENT_HEIGHT*30)));
+                    System.out.println(i);
+                    button.setBackground(buttonColors[i]);
+                    button.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            System.out.println(button.getText());
+                        }
+                    });
+                    panel.add(button);
+                }
+            }
+            return panel;
+        }
+        public JPanel textFeildMania(){
+            JPanel panel = new JPanel();
+
+            //block
+                JPanel bp = new JPanel(new FlowLayout());
+                JLabel bl = new JLabel("Block:");
+                JTextField bt = new JTextField(10);
+                bt.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        System.out.println("bro blocked with just"+bt.getText());
+                        
+                    }
+                });
+                bp.add(bl);
+                bp.add(bt);
+        
+
+
+            panel.add(bp);
+            return panel;
+        }   
     }
 }
+
 
 public class Main 
 {
     public static void main(String[] args) 
     {
         frame f = new frame();
-        String[] hurtFulMessages = {"Tyler smells", "chatGPT is bad at it's job", "severe copium defficiency","L+RATIO+CLOWN"};
-        for (int i = 0; i < 100; i++) {
-            System.out.println(hurtFulMessages[(int)(Math.random() * hurtFulMessages.length)]);
-        }
+
     }
 }
 
