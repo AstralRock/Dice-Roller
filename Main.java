f9df0004d60cc9b0be44cb443e4e9429071c6e21
AstralRock <Purplerock408@gmail.com>
2023-11-30 12:28:33 -0800

    added libraries for it to work(maybe)

--- Main.java
+++ Main.java
@@ -1,25 +1,13 @@
 import java.util.Scanner;
-
+//import javaFX
+import javafx.application.*;
 
 
 class Main
 {
     public static void main(String[] args)
     {
-        //clear terminal regex
-        System.out.print("\n\n\n\n");
-        System.out.println("Welcome to the game!");
-        System.out.println("What is your name?");
-        Logic l = new Logic("Xander Checker");
-
-        for (int i = 0; i < 40; i++) {
-            
-            System.out.println( l.magicHeal());
-
-            System.out.println("AGAIN!! \n");
-        }
-
-
+        
     }
 }
 
