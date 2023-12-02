e9726606817cd70c40e9e00b926ca26f27872468
AstralRock <purplerock408@gmail.com>
2023-12-02 00:17:53 -0800

    Add Logic class and attack method

--- src/main/java/org/openjfx/Main.java
+++ src/main/java/org/openjfx/Main.java
@@ -3,5 +3,8 @@ package org.openjfx;
 public class Main {
     public static void main(String[] args) {
         App.main(args);
+
+        Logic l = new Logic("Ethan Checker");
+        l.attack(0);
     }
 }
